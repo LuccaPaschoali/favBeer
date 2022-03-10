31456a871adf69c6590d66de6269b049e53dc888
LuccaPaschoali <luccapaschoali07@gmail.com>
2022-03-09 22:52:03 -0300

    small fixes

--- beerFav.py
+++ beerFav.py
@@ -5,34 +5,34 @@ i = 0
 #username
 while True:
     try:
-        nm_user = input("Olá, qual o seu nome?\n")
+        nm_user = input(f"Olá, qual o seu nome? ")
         
     except ValueError:
-        print("Desculpe, pode repetir? (Apenas texto!)\n")
+        print(f"Desculpe, pode repetir? (Apenas texto!)")
         continue
         
     if nm_user.isalpha() == True:
         break
         
     else:
-        print("Desculpe, pode repetir? (Apenas texto!)\n")
+        print(f"Desculpe, pode repetir? (Apenas texto!)")
         continue
         
 
 #age check
 while True:
     try:
-        idade = input("Você é maior de Idade?(s/n)\n")
+        idade = input(f"Você é maior de Idade?(s/n) ")
         
     except ValueError:
-        print("Desculpe, proibido o consumo para menores de 18 anos!\n")
+        print(f"Desculpe, proibido o consumo para menores de 18 anos!")
         continue
         
     if idade == "s":
         break
         
     else:
-        print("Desculpe, proibido o consumo para menores de 18 anos!\n")
+        print(f"Desculpe, proibido o consumo para menores de 18 anos!")
         continue
 
 print("Bem vindo(a)", nm_user,"!\n")
@@ -43,39 +43,32 @@ df = pd.read_csv('cervejas_ambev.csv',encoding='ISO-8859-1')
 
 while True:
     try:
-        nm_cerveja = input("Gostaria de saber mais sobre a sua cerveja favorita? Digite o nome dela:\n")
+        nm_cerveja = input(f"Gostaria de saber mais sobre a sua cerveja favorita? Digite o nome dela: ")
         cervejas_filtro = df[df['nome_comp'].str.contains(nm_cerveja, na = False)]
         n_linhas = cervejas_filtro.shape[0]
         
     except ValueError:
-        print("Desculpe, não achamos o item.")
+        print(f"Desculpe, não achamos o item.")
         del cervejas_filtro
         continue
         
     if n_linhas == 1:
-        print('\n')
-        print(cervejas_filtro.iloc[0]['nome_comp'],"\n")
-        print(cervejas_filtro.iloc[0]['tipo'],"\n")
-        print('\n')
-        print("Teor alcoólico: ", cervejas_filtro.iloc[0]['%teor_alc'],"%\n")
-        print("IBU: ", cervejas_filtro.iloc[0]['IBU_amargor'],"\n")
-        print("Ingredientes:", cervejas_filtro.iloc[0]['ingredientes'],"\n")
-        print('\n')
-        print(cervejas_filtro.iloc[0]['descr'],"\n")
+        print(cervejas_filtro.iloc[0]['nome_comp'])
+        print(cervejas_filtro.iloc[0]['tipo'])
+        print(f"Teor alcoólico: {cervejas_filtro.iloc[0]['%teor_alc']}")
+        print(f"IBU: {cervejas_filtro.iloc[0]['IBU_amargor']}")
+        print(f"Ingredientes:{cervejas_filtro.iloc[0]['ingredientes']}")
+        print(f"{cervejas_filtro.iloc[0]['descr']}")
         break
         
     elif n_linhas == 0:
-        print('\n')
-        print("Desculpe, não achamos o item.")
+        print(f"Desculpe, não achamos o item.")
         del cervejas_filtro
         continue
         
     elif n_linhas > 1:
-        print('\n')
-        print("Por favor seja mais específico, qual desses itens você escolheu?")
-        print('\n')
         while i < n_linhas:
             print(cervejas_filtro.iloc[i]['nome_comp'])
             i = i + 1
-            print('\n')
+        print(f"Por favor seja mais específico, qual desses itens você escolheu?")
 
